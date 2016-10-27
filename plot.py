d7c0d74715113dd62d6a5c6b9d70f0215a39b52e
vadimpilyugin <vadimpilyugin@gmail.com>
2016-10-27 16:11:02 +0300

    fixed scale

--- plot.py
+++ plot.py
@@ -18,17 +18,16 @@ x_right = config.getfloat('section', 'x_right')
 x_steps = config.getfloat('section', 'x_steps')
 
 pylab.ion()
-pylab.xlim([x_left,x_right])
-pylab.ylim([0,20])
+x_array = numpy.linspace(x_left,x_right,x_steps)
 lines = open(sys.argv[1], 'r').readlines()
 for line in lines:
 	parts = line.split(' ')
-	x_array = numpy.linspace(x_left,x_right,x_steps)
 	y_array = parts[0:-1]
 	pylab.clf()
+	pylab.xlim([x_left,x_right])
+	pylab.ylim([-20,20])
 	pylab.plot(x_array, y_array)
 	pylab.draw()
-	time.sleep(0.0001)
 pylab.close()
 	
 
